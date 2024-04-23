df373458a0dab048101b416775cd9f432a443dac
appledev116 <williambennett116ok@proton.me>
2024-04-23 11:04:28 -0700

    footerchange

--- src/ourpatners.js
+++ src/ourpatners.js
@@ -1,27 +1,28 @@
-import auditImg from "./components/Image/audit.svg"
-import auditbuttonImg from "./components/Image/auditbutimg.svg"
+import levelImg from "./components/Image/level.svg"
+import handShakeImg from "./components/Image/handshake 1.svg"
+
 const Patner = () => {
     return (
-        <div className="rounded-[15px] bg-white w-[328px] h-[172px] border-[#F4F4F4] border-[1px] p-[20px] mt-[15px]">
-            <div className="h-1/3 flex justify-between items-">
-                <div className="flex justify-between">
-                    <div className="flex justify-between">
-                        <img className="w-[30px] h-[30px] mr-[10px]" src={auditImg}></img>
-                        <a className="font-bold text-[16px]">Audit</a>
-                    </div>
-
+        <div className="rounded-[15px] bg-white w-[1250px] h-[119px] border-[#F4F4F4] border-[1px] p-3 m-[290px] mt-[15px]">
+            <div className="h-1/3 flex justify-between">
+                <div className="flex">
+                    <img  src={handShakeImg}></img>
+                    <a className="ml-2">Our Patners</a>
                 </div>
-
+                <div>
+                    <img src={levelImg}></img>
+                </div>
             </div>
-            <div className="text-[#BBBBBB]">
-                Our Smart Contract is Securely Audited for Reliability.
-            </div>
-            <div className="flex h-1/3  border-[#F4F4F4] mt-3">
-                <button className="flex border-[#F4F4F4] border-[2px] rounded-[12px] w-[145px] h-[40px] items-center justify-center shadow-sm">
-                    <img src={auditbuttonImg} />
-                    <a className="ml-[6px] font-bold">View Aduit</a>
-
-                </button>
+            <div className="flex h-1/2  border-[#F4F4F4] mt-3">
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
+                <div className="w-[120px] h-[40px] bg-[#F1F1F1] mr-[17px]"></div>
             </div>
         </div>
 
