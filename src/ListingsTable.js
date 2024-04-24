df812013d4b84ab2e058aa4bb4b0bdf7b401d788
appledev116 <williambennett116ok@proton.me>
2024-04-24 14:10:31 -0700

    changed scrollbar

--- src/ListingsTable.js
+++ src/ListingsTable.js
@@ -3,8 +3,9 @@ import approvdeImg from "./components/Image/approvedicon.svg"
 import pendingimg from "./components/Image/pendingicon.svg"
 const ListingTable = () => {
     return (
-        <div className="p-[20px] border-[1px] lg:w-[440px] md:w-[440px] w-full h-[418px] rounded-[20px] mt-4 overflow-auto scrollbar-thin">
-            <a className="absolute text-[20px] font-bold  font-[InterRegular] bg-white w-[400px] mt-[-20px] h-[30px] mr-[20px]">Listing</a>
+        <div className="p-[20px] border-[1px] lg:w-[440px] md:w-[440px] w-full rounded-[20px] mt-4 overflow-y-scroll max-h-96 h-fit">
+            
+            <a className="absolute text-[20px] font-bold  font-[InterRegular] bg-white w-[400px] rounded-tr-[20px] mt-[-20px] h-[33px] mr-[20px]">Listing</a>
             <div className="pt-2 mt-[8px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
                 <div className="flex">
                     <a className=" font-[InterRegular]">CoinMarketCap</a>
@@ -109,6 +110,196 @@ const ListingTable = () => {
                 <div className="flex">
                     <a className=" font-[InterRegular]">BitMart</a>
                     <img className="ml-[10px]" src={pendingimg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]" >Dextools</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">Avedex</a>
+                    <img className="ml-[10px]" src={pendingimg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">Toolbit</a>
+                    <img className="ml-[10px]" src={pendingimg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">BitMart</a>
+                    <img className="ml-[10px]" src={pendingimg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">CoinGecko</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">DexView</a>
+                    <img className="text-[14px] ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">CoinGecko</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">DexView</a>
+                    <img className="text-[14px] ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">CoinGecko</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">DexView</a>
+                    <img className="text-[14px] ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">CoinGecko</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">DexView</a>
+                    <img className="text-[14px] ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">CoinGecko</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">DexView</a>
+                    <img className="text-[14px] ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">CoinGecko</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">DexView</a>
+                    <img className="text-[14px] ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[8px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">CoinMarketCap</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">CoinGecko</a>
+                    <img className="ml-[10px]" src={approvdeImg}></img>
+                </div>
+                <div className="flex">
+                    <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
+                    <a className="text-[14px] ml-[6px] text-[#AAAAAA] font-[InterRegular]">06:00 UTC</a>
+                </div>
+            </div>
+            <div className="pt-2 mt-[10px] flex justify-between pb-[8px] border-b border-[#EFEFEF]">
+                <div className="flex">
+                    <a className=" font-[InterRegular]">DexView</a>
+                    <img className="text-[14px] ml-[10px]" src={approvdeImg}></img>
                 </div>
                 <div className="flex">
                     <a className="text-[14px] font-[InterRegular]">28/05/2024</a>
